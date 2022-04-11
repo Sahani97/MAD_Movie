573e30326efcc25652f30cc45cc20124ef1cd5fd
Sahani <sahani070797@gmail.com>
2022-04-11 10:53:35 +0200

    Lifecycle methods added

--- app/src/main/java/com/example/movierow/MainActivity.kt
+++ app/src/main/java/com/example/movierow/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.movierow
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -23,9 +24,43 @@ class MainActivity : ComponentActivity() {
             TopBar {
                 MovieNavigation()
             }
-
         }
+
+        Log.d("MainActivity", "onCreate called")
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy called")
+    }
+
+
 }
 
 @Composable
